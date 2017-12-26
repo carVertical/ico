a84df73eba6692c14956bf1c6053ba84f63e0173
Audrius <urza@nabaka.com>
2017-12-26 13:43:55 +0200

    Added real wallets

--- migrations/3_initial_ico.js
+++ migrations/3_initial_ico.js
@@ -1,10 +1,9 @@
 var cVTokenCrowdsale = artifacts.require('./cVTokenCrowdsale.sol');
 var cVToken = artifacts.require('./cVToken.sol');
 
-const wallet = '0x72fee04528dff07c9b2a702DEb45B0B91636cD76';
+const wallet = '0xe5fc65B4943c548622e98B43E19E596Fc0adA34A';  // Real collection wallet
 const period = 100;  // 100 days
-// const startTime = 1514311200;  // 12/26/2017 @ 6:00pm (UTC)
-const startTime = 1513585566;
+const startTime = 1514311200;  // 12/26/2017 @ 6:00pm (UTC)
 
 module.exports = function(deployer) {
   deployer.deploy(cVToken).then(function() {
